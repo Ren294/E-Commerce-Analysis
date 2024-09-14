19198ccb4896d7e5ba0fa1785e2c256e33a34949
Rennn <76911040+Ren294@users.noreply.github.com>
2024-09-14 22:51:07 +0700

    Update after-compose.sh

--- after-compose.sh
+++ after-compose.sh
@@ -7,3 +7,5 @@ docker exec -i db-ecom pg_restore -U postgres -d ecom /home/ecom_backup.sql
 docker exec -it spark-master bash -c "service ssh start"
 
 docker exec -it cassandra1 /opt/cassandra/bin/cqlsh -f /home/setup/setup.cql
+
+docker exec -it spark-master bash -c "start-thriftserver.sh"
