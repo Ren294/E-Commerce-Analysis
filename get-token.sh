2d51cb92b8134156323d9becae3a85635cd88a03
Rennn <76911040+Ren294@users.noreply.github.com>
2024-09-15 09:01:27 +0700

    Update get-token.sh

--- get-token.sh
+++ get-token.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it jupyter-pyspar jupyter server list | awk -F'token=' '{print $2}' | awk -F'/' '{print $1}' | sed 's/ ::.*//'
+docker exec -it jupyter-pyspark jupyter server list | awk -F'token=' '{print $2}' | awk -F'/' '{print $1}' | sed 's/ ::.*//'
