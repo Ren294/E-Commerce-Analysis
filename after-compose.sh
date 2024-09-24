a383afe8790d60154f4bcb054ab6426ffb0721dd
Rennn <76911040+Ren294@users.noreply.github.com>
2024-09-24 23:43:09 +0700

    Update after-compose.sh

--- after-compose.sh
+++ after-compose.sh
@@ -4,7 +4,7 @@ docker exec -it namenode bash -f /home/setup/setup.sh
 
 docker exec -i db-ecom pg_restore -U postgres -d ecom /home/ecom_backup.sql
 
-docker exec -it spark-master bash -c "service ssh start"
+# docker exec -it spark-master bash -c "service ssh start"
 
 docker exec -it cassandra1 /opt/cassandra/bin/cqlsh -f /home/setup/setup.cql
 
