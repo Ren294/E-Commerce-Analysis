445bd218cbf3ad4f6c30b12441b8bc8c9f339416
Rennn <76911040+Ren294@users.noreply.github.com>
2024-09-25 23:31:43 +0700

    Delete spark directory

--- spark/apps/streaming/logStreaming.sh
+++
@@ -1,3 +0,0 @@
-spark-submit --master spark://spark-master:7077 \
-    --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,org.apache.kafka:kafka-clients:3.2.0 \
-    /opt/spark-apps/streaming/logStreaming.py &> /opt/spark/logs/logStreaming.log
