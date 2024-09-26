5ad543b5e62f55bea3ce10f357783ae4a2142ca6
Rennn <76911040+Ren294@users.noreply.github.com>
2024-09-26 09:57:35 +0700

    Update spark apps

---
+++ spark/apps/vacuum/log/vacuum_log.py
@@ -0,0 +1,19 @@
+from delta.tables import DeltaTable
+from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
+
+builder = SparkSession.builder \
+    .appName("Optimize and Vacuum Delta Table logs.Ecom_log") \
+    .config("spark.sql.warehouse.dir", "/user/hive/warehouse") \
+    .config("spark.sql.catalogImplementation", "hive") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.0.0")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+delta_table = DeltaTable.forName(spark, "logs.Ecom_log")
+
+delta_table.vacuum(retentionHours=168)
+
+spark.stop()
